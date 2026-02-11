78e2924ee63050ab4598daa8c989570046d3b576
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-02-11 14:28:25 +0000

    Added initial graph/systme gen

---
+++ src/worldgen/graph.py
@@ -0,0 +1,61 @@
+from system import system
+from random import randint
+
+'''
+A graph class that builds itself into the world map.
+Systems are created and filled out with attributes according to user input.
+'''
+
+class Graph:
+    def __init__(self, density, layers):
+        # Instantiates variables
+        self.id_count = 1
+        self.systems = []
+        self.density = density
+        self.layers = layers
+
+        # Runs setup function
+        self.setup()
+
+
+    def setup(self):
+        # Function to generate the world
+
+        start_node = system(self.id_count,0)
+        self.systems.append(start_node)
+
+        self.systemGen(start_node,1)
+
+        pass
+
+
+    def systemGen(self, source, layer):
+        # Recursive function to create systems according to layers and density
+
+        if layer > self.layers:
+            # Breaks out if layer limit has been reached
+            return
+
+        for i in range(self.density):
+            # A new system is created and added to the node list
+            self.id_count += 1
+            node = system(self.id_count,layer)
+
+            # Relevant adjacencies are are added
+            distance = randint(2,12)
+            node.adjacency.append({"target" : source.id,
+                                   "distance" : distance})
+            source.adjacency.append({"target" : node.id,
+                                     "distance" : distance})
+
+            # Adds the new node to the list and recurs
+            self.systems.append(node)
+            self.systemGen(node,layer+1)
+
+
+if __name__ == "__main__":
+    # Gets inputs from user
+    my_graph = Graph(2,3)
+
+    if my_graph.systems[1].id != my_graph.systems[2].id:
+        print("yay!")
---
+++ src/worldgen/system.py
@@ -0,0 +1,9 @@
+class system:
+    def __init__(self, sysID, layer):
+        # Define initial system stats (placeholder)
+
+        self.id = sysID
+        self.adjacency = []
+        self.sec = False
+        self.tier = 1
+        self.layer = layer
