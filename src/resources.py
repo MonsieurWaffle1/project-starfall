33a39a5c5cde996ec22859a55964ee22f2e05d42
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-02-12 19:43:10 +0000

    Reformatted to prevent errors

--- src/resources.py
+++ src/resources.py
@@ -1,13 +1,13 @@
 class Commodity:
-    def init(self, name, tier):
+    def __init__(self, name, tier):
         self.name: str = name
         self.tier: int = tier
 
 class Resource(Commodity):
     def __init__(self, name):
-        super.__init__(name, 1)
+        super().__init__(name, 1)
 
 class Alloy(Commodity):
     def __init__(self, name, cost, tier):
-        super.__init__(name, tier)
+        super().__init__(name, tier)
         self.cost: tuple = cost
