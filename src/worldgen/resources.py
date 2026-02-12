04605fc7e653505a803ab662d6bee628ddb86836
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-02-12 19:22:42 +0000

    Added resource classes

---
+++ src/worldgen/resources.py
@@ -0,0 +1,13 @@
+class Commodity:
+    def init(self, name, tier):
+        self.name: str = name
+        self.tier: int = tier
+
+class Resource(Commodity):
+    def __init__(self, name):
+        super.__init__(name, 1)
+
+class Alloy(Commodity):
+    def __init__(self, name, cost, tier):
+        super.__init__(name, tier)
+        self.cost: tuple = cost
