599a5381968f9ff403ab2a831b8e6375269bb2a3
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-02-12 19:23:15 +0000

    Replaced main.py with app.py at /src

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from worldgen.graph import Graph
+
+
+
+if __name__ == "__main__":
+    # Gets inputs from user
+    my_graph = Graph(density = 2, layers = 4)
+
+    for system in my_graph.systems:
+        print(system.layer)
