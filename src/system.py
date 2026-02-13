e5ce58354abaa8e9d74cc4e5a32149689ba18f65
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-02-13 11:36:20 +0000

    Added security assignment

--- src/system.py
+++ src/system.py
@@ -6,13 +6,21 @@ class System:
 
         self.id = sysID
         self.adjacency = []
-        self.sec = False
+        self.sec = True
         self.tier = 1
         self.layer = layer
         self.resources = []
 
     def assignRandom(self, tiers:tuple):
         self.tier = random.choice(tiers)
+        self.secAssign()
+
 
     def assign(self,tier:int):
         self.tier = tier
+        self.secAssign()
+
+    def secAssign(self):
+        # Changes to low sec if T1 system
+        if self.tier == 1:
+            self.sec = False
