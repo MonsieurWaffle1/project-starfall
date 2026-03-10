05fc69d635d5fc34b285b6243c972da11f478514
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-03-10 12:49:49 +0000

    Added station building + comments

--- src/buildings.py
+++ src/buildings.py
@@ -8,10 +8,21 @@ class Building:
         self.name = name
         self.cost = cost
 
+class Station(Building):
+    def __init__(self):
+        # Appears in every system, holds a system's market
+        cost = {}
+
+        # Generates a name for the station based on a list of available options
+        name = f"{choice(system_names)} {choice(station_names)}"
+        super().__init__(name, cost)
+
 
 class Refinery(Building):
     def __init__(self, level):
+        # Refines ores into usable bars
         if level == "advanced":
+            # Advanced level is able to create alloys
             name = "Advanced"
             cost = {
                 "t3_components":3
@@ -37,6 +48,7 @@ class BasicRefinery(Refinery):
 
 
 class ManufacturingPlant(Building):
+    # Used to create mechanical components
     def __init__(self):
         cost = {
             "t3_components":5
