b845acdc3daa9fdd81b3ce2366c40afdb5935138
Theo Smith <131392716+MonsieurWaffle1@users.noreply.github.com>
2026-03-13 14:10:29 +0000

    Updated app.py to allow for market testing

--- src/app.py
+++ src/app.py
@@ -1,10 +1,35 @@
-from src.graph import Graph
-
-
+from graph import Graph
+from system import System
 
 if __name__ == "__main__":
     # Gets inputs from user
-    my_graph = Graph(density = 2, layers = 4)
+    my_graph = Graph(density = 3, layers = 3)
 
-    for building in my_graph.buildings:
-        print(building)
+    my_system = True
+
+    tier = int(input("What tier? "))
+
+    for i in my_graph.systems:
+        if i.tier == tier:
+            my_system = i
+            break
+
+    my_system.market.update()
+
+    for product in my_system.market.products:
+        print(f"{product.name}, {product.quantity}, {product.price}")
+
+    while True:
+        action = input(f"Buy or sell? ")
+        prod = input("What product? ")
+        quant = int(input("What quantity? "))
+
+        my_system.market.adjust(action, prod, quant)
+        my_system.market.update()
+
+        print("")
+
+        for product in my_system.market.products:
+            print(f"{product.name}, {product.quantity}, {product.price}")
+
+
